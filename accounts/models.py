8b69094c770a6f65c1c0a36fd1dd6145f7e31ffa
Arshia <arshiashirvani.f1385@gmail.com>
2024-08-28 04:48:05 +0330

    We made it optional for some fields

--- accounts/models.py
+++ accounts/models.py
@@ -1,6 +1,4 @@
 from django.db import models
-
-
 from typing import Any
 from django.db import models
 from django.contrib.auth.models import (AbstractBaseUser,BaseUserManager,PermissionsMixin)
@@ -72,18 +70,20 @@ class Profile(models.Model):
     first_name = models.CharField(max_length=250)
     last_name = models.CharField(max_length=255)
     image = models.ImageField(blank=True,null=True)
-    description = models.TextField()
-    instagram_link = models.URLField(max_length=255)
-    github_link = models.URLField(max_length=255)
-    linkedin_link = models.URLField(max_length=255)
-    gmail_link = models.URLField(max_length=255)
+    description = models.TextField(blank=True)
+    instagram_link = models.URLField(max_length=255,null=True,blank=True)
+    github_link = models.URLField(max_length=255,null=True,blank=True)
+    linkedin_link = models.URLField(max_length=255,null=True,blank=True)
+    gmail_link = models.URLField(max_length=255,null=True,blank=True)
 
     created_date = models.DateTimeField(auto_now_add=True)
     updated_date = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.user.email
-    
+
+
+'''when user was created , An object is automatically created inside the profile '''
 @receiver(post_save, sender=User)
 def save_profile(sender,instance,created,**kwargs):
     if created:
