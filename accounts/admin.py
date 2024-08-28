a3af63eff5d67cdc4dc86c6a8b7e9c6ea01a7808
Arshia <arshiashirvani.f1385@gmail.com>
2024-08-28 04:42:41 +0330

    Created user model and register in project
    But it works like this gets email instead of username and the email is also unique for each user and when user was created , An object is automatically created inside the profile

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,46 @@
 from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from accounts.models import User,Profile
+
+class CustomUserAdmin(UserAdmin):
+    model=User
+    list_display=('email', 'is_superuser', 'is_active',)
+    list_filter=('email', 'is_active',)
+    search_fields = ('email',)
+    ordering = ('email',)
+    fieldsets = (
+        ('Authenticaation',{
+            "fields":(
+                'email','password'
+            ),
+        }),
+        ('Permission',{
+            "fields":(
+                'is_staff','is_active','is_superuser'
+            ),
+        }),
+        ('group permissions',{
+            "fields":(
+                'groups','user_permissions',
+            ),
+        }),
+        ('Important Date',{
+            "fields":(
+                'last_login',
+            ),
+        }),
+    )
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "fields": (
+                "email", "password1", "password2", "is_staff",
+                "is_active", "is_superuser",
+            )}
+        ),
+    )
+
+admin.site.register(User,CustomUserAdmin)
+admin.site.register(Profile)
